7442d1c2336e549ea0ce570072d359ee396205d9
Verta-master <vertyagina@gmail.com>
2022-02-18 12:10:41 +0600

    Update 2022-02-18T06:10:36.810Z

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
 //Mobile menu
-//$('.menu__btn').click(function() {
-//  $('.menu').toggleClass('menu--opened');
-//  $('.menu__list').slideToggle();
-//})
-//
-//if (window.innerWidth < 766) {
-//  $('.menu__link').click(function() {
-//    $('.menu__list').slideToggle();
-//    $('.menu').removeClass('menu--opened');
-//  })
-//}
+$('.header__menu').click(function() {
+  $('.mobile').addClass('mobile--opened');
+});
+
+$('.mobile__close').click(function() {
+  $('.mobile').removeClass('mobile--opened');
+});
 
 //Cart scroll
 $('.cart__wrap').niceScroll();
